1a5ce8997ed926fab7bb4d893b8cdeaf526053f6
André Hopf <andre.hopf@gmail.com>
2023-06-20 15:23:00 -0300

    Exercicio 1 semana 3

---
+++ semana3/ex1.sql
@@ -0,0 +1,23 @@
+/*Criar uma função que calcule o desconto do produto, 
+passar os parâmetros código do produto e aplicarDesconto boolean
+
+Regra
+Fazer um select bucando o valor do produto
+Se a parametro aplicarDseconto for igual verdadeiro aplicar desconto de 10%*/
+
+CREATE OR REPLACE FUNCTION CalcularDesconto(
+produto_id PRODUTO.ID%TYPE,
+aplicar_desconto BOOLEAN) 
+RETURN NUMBER IS
+    v_valor NUMBER;
+BEGIN 
+    SELECT valor INTO v_valor FROM produto WHERE id = produto_id;
+    
+    IF aplicar_desconto = true THEN
+        RETURN v_valor*0.9;
+    ELSE
+        RETURN v_valor;
+    END IF;
+
+END;
+    
